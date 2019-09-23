581c4e286504d4b49d57d78c8cb2aebd6b320e1e
Antaine <aconn@outlook.com>
2019-09-23 23:40:41 +0100

    More Accurate

--- lab2/approxSqrt.py
+++ lab2/approxSqrt.py
@@ -4,7 +4,7 @@ def sqrt(x):
     z = 1.0
     #Keep getting better estimate for the square root
     #of x, until you are within two decimal places.
-    while abs(z*z - x)>= 0.01:
+    while abs(z*z - x)>= 0.0001:
         #Get a better approximation for the square root.
         z -= (z*z - x) / (2*z)
     return z
